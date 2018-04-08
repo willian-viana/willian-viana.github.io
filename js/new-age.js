2982c953aaa60ddbdd0755151f5d02bd080f5e0e
Willian Viana <willian.batista.viana@gmail.com>
2018-04-08 20:16:42 -0300

    chore(real-images): Add real images of beers and replace social links

--- js/new-age.js
+++ js/new-age.js
@@ -43,7 +43,6 @@
     window.location.href = 'https://google.com'
   })
 
-
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
